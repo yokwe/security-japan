010e0f990404695ec273af0f16ea9374434f411a
hasegawa <hasegawa@ubuntudev.local>
2019-11-20 16:08:39 +0900

    Add EDINET and UpdateEDINET class.

---
+++ src/yokwe/security/japan/fsa/EDINET.java
@@ -0,0 +1,64 @@
+package yokwe.security.japan.fsa;
+
+import yokwe.util.CSVUtil;
+import yokwe.util.libreoffice.Sheet;
+
+public class EDINET extends Sheet implements Comparable<EDINET>{
+	public static final String URL_DOWNLOAD     = "https://disclosure.edinet-fsa.go.jp/E01EW/download?uji.verb=W1E62071EdinetCodeDownload&uji.bean=ee.bean.W1E62071.EEW1E62071Bean&TID=W1E62071&PID=W1E62071&SESSIONKEY=9999&downloadFileName=&lgKbn=2&dflg=0&iflg=0&dispKbn=1";
+	public static final String CHARSET_DOWNLOAD = "MS932";
+	
+	public static final String PATH_DOWNLOAD    = "tmp/download/Edinetcode.zip";
+	public static final String PATH_DATA        = "tmp/data/EdinetcodeDlInfo.csv";
+	
+	@CSVUtil.ColumnName("ＥＤＩＮＥＴコード")
+	public String edinetCode;
+	
+	@CSVUtil.ColumnName("提出者種別")
+	public String category;
+
+	@CSVUtil.ColumnName("上場区分")
+	public String listedIssue;
+
+	@CSVUtil.ColumnName("連結の有無")
+	public String hasConsolidateSubsidary;
+
+	@CSVUtil.ColumnName("資本金")
+	public String capital;
+	
+	@CSVUtil.ColumnName("決算日")
+	public String closingDate;
+	
+	@CSVUtil.ColumnName("提出者名")
+	public String name;
+	
+	@CSVUtil.ColumnName("提出者名（英字）")
+	public String nameEnglish;
+	
+	@CSVUtil.ColumnName("提出者名（ヨミ）")
+	public String nameRuby;
+	
+	@CSVUtil.ColumnName("所在地")
+	public String address;
+	
+	@CSVUtil.ColumnName("提出者業種")
+	public String sector;
+	
+	@CSVUtil.ColumnName("証券コード")
+	public String stockCode;
+	
+	@CSVUtil.ColumnName("提出者法人番号")
+	public String corporateCode;
+	
+	@Override
+	public String toString() {
+		return String.format("%s %s %s %s %s %s %s %s %s %s %s %s %s",
+				edinetCode, category, listedIssue, hasConsolidateSubsidary, capital, closingDate,
+				name, nameEnglish, nameRuby, address, sector, stockCode, corporateCode);
+	}
+	
+	@Override
+	public int compareTo(EDINET that) {
+		int ret = this.edinetCode.compareTo(that.edinetCode);
+		return ret;
+	}
+}
