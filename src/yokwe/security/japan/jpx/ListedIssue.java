7a5c460aeebc73adb50f9e27a4bc45e2becd1b7f
hasegawa <hasegawa@ubuntudev.local>
2019-11-22 16:11:00 +0900

    Move spread sheet related code to UpdateListedIssue class.

--- src/yokwe/security/japan/jpx/ListedIssue.java
+++ src/yokwe/security/japan/jpx/ListedIssue.java
@@ -1,62 +1,47 @@
 package yokwe.security.japan.jpx;
 
+import java.util.List;
+
 import yokwe.util.CSVUtil;
-import yokwe.util.libreoffice.Sheet;
-import yokwe.util.libreoffice.SpreadSheet;
 
-
-@Sheet.SheetName("Sheet1")
-@Sheet.HeaderRow(0)
-@Sheet.DataRow(1)
-public class ListedIssue extends Sheet implements Comparable<ListedIssue> {	
+public class ListedIssue implements Comparable<ListedIssue> {	
 	public static final String URL_DOWNLOAD  = "https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls";
-	public static final String PATH_DOWNLOAD = "tmp/download/ListedIssue.xls";
-	public static final String PATH_DATA     = "tmp/data/listedIssue.csv";
+	public static final String PATH_DOWNLOAD = "tmp/download/listed-issue.xls";
+	public static final String PATH_DATA     = "tmp/data/listed-issue.csv";
 	
 	public static final String MARKET_ETF    = "ETF・ETN";
+	
+	public static List<ListedIssue> load() {
+		return CSVUtil.read(ListedIssue.class).file(PATH_DATA);
+	}
 
-	@Sheet.ColumnName("日付")
 	@CSVUtil.ColumnName("日付")
-	@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
-	public String date;
+	public String date; // YYYY-MM-DD
 	
-	@Sheet.ColumnName("コード")
 	@CSVUtil.ColumnName("コード")
-	@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
-	public String stockCode;
+	public String stockCode; // NNNN - four digits
 	
-	@Sheet.ColumnName("銘柄名")
 	@CSVUtil.ColumnName("銘柄名")
 	public String name;
 	
-	@Sheet.ColumnName("市場・商品区分")
 	@CSVUtil.ColumnName("市場・商品区分")
 	public String market;
 	
-	@Sheet.ColumnName("33業種コード")
-	@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
 	@CSVUtil.ColumnName("33業種コード")
 	public String sector33Code;
 	
-	@Sheet.ColumnName("33業種区分")
 	@CSVUtil.ColumnName("33業種区分")
 	public String sector33;
 	
-	@Sheet.ColumnName("17業種コード")
-	@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
 	@CSVUtil.ColumnName("17業種コード")
 	public String sector17Code;
 	
-	@Sheet.ColumnName("17業種区分")
 	@CSVUtil.ColumnName("17業種区分")
 	public String sector17;
 	
-	@Sheet.ColumnName("規模コード")
-	@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
 	@CSVUtil.ColumnName("規模コード")
 	public String scaleCode;
 	
-	@Sheet.ColumnName("規模区分")
 	@CSVUtil.ColumnName("規模区分")
 	public String scale;
 	
--- src/yokwe/security/japan/jpx/UpdateListedIssue.java
+++ src/yokwe/security/japan/jpx/UpdateListedIssue.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.slf4j.LoggerFactory;
 
+import yokwe.UnexpectedException;
 import yokwe.util.CSVUtil;
 import yokwe.util.FileUtil;
 import yokwe.util.HttpUtil;
@@ -16,8 +17,49 @@ import yokwe.util.libreoffice.SpreadSheet;
 
 public class UpdateListedIssue {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(UpdateListedIssue.class);
+	
+	@Sheet.SheetName("Sheet1")
+	@Sheet.HeaderRow(0)
+	@Sheet.DataRow(1)
+	public static class RawData extends Sheet {	
+		@Sheet.ColumnName("日付")
+		@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
+		public String date;
+		
+		@Sheet.ColumnName("コード")
+		@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
+		public String stockCode;
+		
+		@Sheet.ColumnName("銘柄名")
+		public String name;
+		
+		@Sheet.ColumnName("市場・商品区分")
+		public String market;
+		
+		@Sheet.ColumnName("33業種コード")
+		@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
+		public String sector33Code;
+		
+		@Sheet.ColumnName("33業種区分")
+		public String sector33;
+		
+		@Sheet.ColumnName("17業種コード")
+		@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
+		public String sector17Code;
+		
+		@Sheet.ColumnName("17業種区分")
+		public String sector17;
+		
+		@Sheet.ColumnName("規模コード")
+		@Sheet.NumberFormat(SpreadSheet.FORMAT_INTEGER)
+		public String scaleCode;
+		
+		@Sheet.ColumnName("規模区分")
+		public String scale;
+	}
 
-	public static void main(String[] args) throws MalformedURLException {
+
+	public static void main(String[] args) {
 		logger.info("START");
 		
 		logger.info("download {}", ListedIssue.URL_DOWNLOAD);
@@ -27,19 +69,33 @@ public class UpdateListedIssue {
 		logger.info("write {} {}", ListedIssue.PATH_DOWNLOAD, result.rawData.length);
 		FileUtil.write().file(ListedIssue.PATH_DOWNLOAD, result.rawData);
 		
-		String url  = new File(ListedIssue.PATH_DOWNLOAD).toURI().toURL().toString();
+		String url;
+		try {
+			url = new File(ListedIssue.PATH_DOWNLOAD).toURI().toURL().toString();
+		} catch (MalformedURLException e) {
+			String exceptionName = e.getClass().getSimpleName();
+			logger.error("{} {}", exceptionName, e);
+			throw new UnexpectedException(exceptionName, e);
+		}
 		logger.info("open {}", url);
 		
 		try (SpreadSheet docListedIssue = new SpreadSheet(url, true)) {
-			List<ListedIssue> list = Sheet.extractSheet(docListedIssue, ListedIssue.class);
-			logger.info("read {}", list.size());
+			List<RawData> rawDataList = Sheet.extractSheet(docListedIssue, RawData.class);
+			logger.info("read {}", rawDataList.size());
 			
 			// Trim space
-			List<ListedIssue> newList = new ArrayList<>(list.size());
-			for(ListedIssue value: list) {
+			List<ListedIssue> newList = new ArrayList<>(rawDataList.size());
+			for(RawData value: rawDataList) {
 				ListedIssue newValue = new ListedIssue();
 				
-				newValue.date         = value.date.trim();
+				String date = value.date.trim();
+				if (date.length() != 8) {
+					logger.error("Unpexpected date value {}!", date);
+					throw new UnexpectedException("Unpexpected date value");
+				}
+				String newDate = String.format("%s-%s-%s", date.substring(0, 4), date.substring(4, 6), date.substring(6, 8));
+				
+				newValue.date         = newDate;
 				newValue.stockCode    = value.stockCode.trim();
 				newValue.name         = value.name.trim();
 				newValue.market       = value.market.trim();
@@ -56,7 +112,7 @@ public class UpdateListedIssue {
 			// Sort before write
 			Collections.sort(newList);
 			
-			logger.info("write {} {}", ListedIssue.PATH_DATA, list.size());
+			logger.info("write {} {}", ListedIssue.PATH_DATA, newList.size());
 			CSVUtil.write(ListedIssue.class).file(ListedIssue.PATH_DATA, newList);
 		}
 		
