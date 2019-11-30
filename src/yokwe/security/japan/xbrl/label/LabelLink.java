5d8d0e802ca47e90e417196b10f04ea225ed6e45
hasegawa <hasegawa@ubuntudev.local>
2019-11-30 12:49:00 +0900

    Remove unused code.

--- src/yokwe/security/japan/xbrl/label/LabelLink.java
+++ src/yokwe/security/japan/xbrl/label/LabelLink.java
@@ -2,23 +2,17 @@ package yokwe.security.japan.xbrl.label;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlEnumValue;
-import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 import org.slf4j.LoggerFactory;
 
 import yokwe.UnexpectedException;
-import yokwe.security.japan.xbrl.label.Label.Role;
 
 public class LabelLink {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(LabelLink.class);
@@ -52,18 +46,9 @@ public class LabelLink {
 	@XmlElement(name = "labelArc")
 	public final List<LabelArc> labelArcList = new ArrayList<>();
 	
-	@XmlTransient
-	public final Map<String, Loc>      locMap      = new TreeMap<>();
-	
-	@XmlTransient
-	public final Map<String, Label>    labelMap    = new TreeMap<>();
-	
-	@XmlTransient
-	public final Map<String, LabelArc> labelArcMap = new TreeMap<>();
-	
 	@Override
 	public String toString() {
-		return String.format("{%s %s loc %d / %d  label %d / %d  labelArc %d / %d}", type, role, locMap.size(), locList.size(), labelMap.size(), labelList.size(), labelArcMap.size(), labelArcList.size());
+		return String.format("{%s %s  loc %d  label %d  labelArc %d}", type, role, locList.size(), labelList.size(), labelArcList.size());
 	}
 	
 	void afterUnmarshal(Unmarshaller u, Object parent) {
@@ -75,52 +60,6 @@ public class LabelLink {
 		if (role == null) {
 			logger.error("role is null %s", this);
 			throw new UnexpectedException("role is null");
-		}
-		
-		locMap.clear();
-		for(Loc e: locList) {
-			if (locMap.containsKey(e.label)) {
-				logger.error("Duplicate label  {}", e.label);
-				logger.error("old {}", locMap.get(e.label));
-				logger.error("new {}", e);
-//				throw new UnexpectedException("Duplicate label");
-			} else {
-				locMap.put(e.label, e);
-			}
-		}
-		
-		labelMap.clear();
-		for(Label e: labelList) {
-			if (labelMap.containsKey(e.label)) {
-				logger.error("Duplicate label  {}", e.label);
-				logger.error("old {}", labelMap.get(e.label));
-				logger.error("new {}", e);
-				throw new UnexpectedException("Duplicate label");
-			} else {
-				labelMap.put(e.label, e);
-			}
-		}
-		
-		labelArcMap.clear();
-		for(LabelArc e: labelArcList) {
-//			if (e.to.matches(".+?_[0-9]+$")) {
-//				logger.info("SKIP {}", e.to);
-//				continue;
-//			}
-			
-			if (labelArcMap.containsKey(e.from)) {
-//				logger.error("Duplicate from  {}", e.from);
-//				LabelArc old = labelArcMap.get(e.from);
-//				Label oldLabel = labelMap.get(old.to);
-//				Label newLabel = labelMap.get(e.to);
-//				logger.error("  old {}", oldLabel);
-//				logger.error("  new {}", newLabel);
-////				logger.error("  old {}", labelArcMap.get(e.from));
-////				logger.error("  new {}", e);
-////				throw new UnexpectedException("Duplicate from");
-			} else {
-				labelArcMap.put(e.from, e);
-			}
 		}
 	}
 }
