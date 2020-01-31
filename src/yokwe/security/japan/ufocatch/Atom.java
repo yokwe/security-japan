843b7ca127d4e599da5c5a7d89307c6957908d46
hasegawa <hasegawa@ubuntudev.local>
2020-01-31 15:11:00 +0900

    Add support for downloading.

--- src/yokwe/security/japan/ufocatch/Atom.java
+++ src/yokwe/security/japan/ufocatch/Atom.java
@@ -1,16 +1,96 @@
 package yokwe.security.japan.ufocatch;
 
+import java.io.File;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.slf4j.LoggerFactory;
 
 import yokwe.UnexpectedException;
+import yokwe.security.japan.ufocatch.atom.Link;
+import yokwe.util.FileUtil;
 import yokwe.util.HttpUtil;
 
 public class Atom {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Atom.class);
 
+	public static final String DIR_BASE = "tmp/ufocatch";
+	
+	public static String getPath(String name) {
+		return String.format("%s/%s", DIR_BASE, name);
+	}
+	public static List<File> getExistingFileList() {
+		File dir = new File(DIR_BASE);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		
+		List<File> ret = new ArrayList<>();
+		for(File file: dir.listFiles()) {
+			String name = file.getName();
+			if (name.endsWith("-ixbrl.htm")) {
+				ret.add(file);
+			}
+		}
+		Collections.sort(ret);
+		return ret;
+	}
+	public static Map<String, File> getExistingFileMap() {
+		File dir = new File(DIR_BASE);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		
+		Map<String, File> ret = new TreeMap<>();
+		for(File file: getExistingFileList()) {
+			String name = file.getName();
+			ret.put(name, file);
+		}
+		
+		return ret;
+	}
+	private static String getFilename(String href) {
+		try {
+			URL url = new URL(href);
+			Path path = Paths.get(url.getPath());
+			return path.getFileName().toString();
+		} catch (MalformedURLException e) {
+			String exceptionName = e.getClass().getSimpleName();
+			logger.error("{} {}", exceptionName, e);
+			throw new UnexpectedException(exceptionName, e);
+		}
+	}
+	public static boolean downloadExists(String href) {
+		String filename = getFilename(href);
+		String path     = getPath(filename);
+		File   file     = new File(path);
+		return file.exists();
+	}
+	public static String download(String href) {
+		String filename = getFilename(href);
+		String path     = getPath(filename);
+		File   file     = new File(path);
+		if (file.exists()) {
+			return FileUtil.read().file(file);
+		} else {
+			HttpUtil.Result result = HttpUtil.getInstance().download(href);
+			if (result.result != null) {
+				FileUtil.write().file(file, result.result);
+			}
+			return result.result;
+		}
+	}
+	
 	public static final String URL_BASE   = "https://resource.ufocatch.com/atom";
 	
 	public static enum Kind {
@@ -38,5 +118,12 @@ public class Atom {
 			logger.error("{} {}", exceptionName, e);
 			throw new UnexpectedException(exceptionName, e);
 		}
+	}
+	
+	private static Link.Relation getLinkMapKey(Link link) {
+		return link.rel;
+	}
+	public static Map<Link.Relation, Link> getLinkMap(List<Link> list) {
+		return list.stream().collect(Collectors.toMap(Atom::getLinkMapKey, o -> (Link)o));
 	}
 }
