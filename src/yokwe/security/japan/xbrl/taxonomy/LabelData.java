4306556059ac93166e9f068c18d66a496194c548
hasegawa <hasegawa@ubuntudev.local>
2020-02-07 14:26:16 +0900

    Add static method add and get.

--- src/yokwe/security/japan/xbrl/taxonomy/LabelData.java
+++ src/yokwe/security/japan/xbrl/taxonomy/LabelData.java
@@ -1,15 +1,50 @@
 package yokwe.security.japan.xbrl.taxonomy;
 
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.slf4j.LoggerFactory;
+
+import yokwe.UnexpectedException;
 import yokwe.util.XMLUtil.QValue;
 
 public class LabelData {
-    public final QValue qName;
-    public final String en;
-    public final String ja;
+	static final org.slf4j.Logger logger = LoggerFactory.getLogger(LabelData.class);
 
-    public LabelData(QValue qName, String en, String ja) {
+	private static final Map<QValue, LabelData> all = new TreeMap<>();
+	
+	public static void add(QValue qName, String en, String ja, Enum<?> e) {
+		LabelData value = new LabelData(qName, en, ja, e);
+		QValue    key   = value.qName;
+		
+		if (all.containsKey(key)) {
+			logger.error("Duplicate key");
+			logger.error("  key   {}", key);
+			logger.error("  value {}", value);
+			throw new UnexpectedException("Duplicate key");
+		} else {
+			all.put(key, value);
+		}
+	}
+	public static LabelData get(QValue key) {
+		if (all.containsKey(key)) {
+			return all.get(key);
+		} else {
+			logger.error("Unknown key");
+			logger.error("  key   {}", key);
+			throw new UnexpectedException("Unknown key");
+		}
+	}
+	
+    public final QValue qName;
+    public final String  en;
+    public final String  ja;
+    public final Enum<?> e;
+
+    private LabelData(QValue qName, String en, String ja, Enum<?> e) {
     	this.qName = qName;
     	this.en    = en;
     	this.ja    = ja;
+    	this.e     = e;
     }
 }
