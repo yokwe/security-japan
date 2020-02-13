63b96b53762bf685322586c42b178e6b071a140d
hasegawa <hasegawa@ubuntudev.local>
2020-02-13 19:36:31 +0900

    Add pdf relate methods.

--- src/yokwe/security/japan/ufocatch/Atom.java
+++ src/yokwe/security/japan/ufocatch/Atom.java
@@ -7,9 +7,15 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import javax.xml.bind.JAXB;
 
@@ -20,12 +26,15 @@ import yokwe.security.japan.jpx.Category;
 import yokwe.security.japan.jpx.FinancialSummary;
 import yokwe.security.japan.ufocatch.atom.Feed;
 import yokwe.util.FileUtil;
+import yokwe.util.HashCode;
 import yokwe.util.HttpUtil;
+import yokwe.util.StringUtil;
 
 public class Atom {
 	static final org.slf4j.Logger logger = LoggerFactory.getLogger(Atom.class);
 
-	private static final String DIR_BASE = "tmp/ufocatch";
+	private static final String DIR_BASE     = "tmp/ufocatch";
+	private static final String DIR_BASE_PDF = "tmp/ufocatch/pdf";
 	
 	public static String getPath(String filename) {
 		{
@@ -37,6 +46,53 @@ public class Atom {
 		logger.error("Unexpected filename {}", filename);
 		throw new UnexpectedException("Unexpected filename");
 	}
+	
+	private static final Pattern PAT_TDID = Pattern.compile(".+?\\/(TD[0-9]{13})$");
+	public static String getTDID(String url) {
+		Matcher m = PAT_TDID.matcher(url);
+		if (m.matches()) {
+			String tdid = m.group(1);
+			return tdid;
+		} else {
+			logger.error("Unexpected url {}", url);
+			throw new UnexpectedException("Unexpected url");
+		}
+	}
+	public static String getPathPDF(String tdid) {
+		return String.format("%s/%s.pdf", DIR_BASE_PDF, tdid);
+	}
+	public static byte[] downloadPDFRawData(String tdid) {
+		String referer = String.format("https://resource.ufocatch.com/pdfpage/%s", tdid);
+		String url = String.format("https://resource.ufocatch.com/pdf/tdnet/%s", tdid);
+		
+		HttpUtil.Result result = HttpUtil.getInstance().withReferer(referer).withRawData(true).download(url);
+		return result.rawData;
+	}
+	private static List<File> fileListPDF = null;
+	public static List<File> getFileListPDF() {
+		if (fileListPDF == null) {
+			fileListPDF = FileUtil.listFile(new File(DIR_BASE_PDF)).stream().filter(o -> o.getName().endsWith(".pdf")).collect(Collectors.toList());
+			Collections.sort(fileListPDF);
+		}
+		return fileListPDF;
+	}
+	private static List<File> fileListUniqueuePDF = null;
+	public static List<File> getFileListUniqueuePDF() {
+		if (fileListUniqueuePDF == null) {
+			Set<String> md5Set = new TreeSet<>();
+			for(File file: FileUtil.listFile(new File(DIR_BASE_PDF)).stream().filter(o -> o.getName().endsWith(".pdf")).collect(Collectors.toList())) {
+				String md5 = StringUtil.toHexString(HashCode.getHashCode(file));
+				if (md5Set.contains(md5)) {
+					// same file content is appeared as other file name.
+				} else {
+					md5Set.add(md5);
+					fileListUniqueuePDF.add(file);
+				}
+			}
+			Collections.sort(fileListUniqueuePDF);
+		}
+		return fileListUniqueuePDF;
+	}	
 	
 	private static List<File> fileList = null;
 	public static List<File> getFileList() {
