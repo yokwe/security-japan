8ade783d6bf1e54dced5d6f19b16a54550f7d1a0
hasegawa <hasegawa@ubuntudev.local>
2020-02-14 13:47:09 +0900

    Add another useful methods from DownloadETFPDF

--- src/yokwe/security/japan/ufocatch/Atom.java
+++ src/yokwe/security/japan/ufocatch/Atom.java
@@ -7,15 +7,11 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import javax.xml.bind.JAXB;
 
@@ -26,15 +22,12 @@ import yokwe.security.japan.jpx.Category;
 import yokwe.security.japan.jpx.FinancialSummary;
 import yokwe.security.japan.ufocatch.atom.Feed;
 import yokwe.util.FileUtil;
-import yokwe.util.HashCode;
 import yokwe.util.HttpUtil;
-import yokwe.util.StringUtil;
 
 public class Atom {
 	static final org.slf4j.Logger logger = LoggerFactory.getLogger(Atom.class);
 
 	private static final String DIR_BASE     = "tmp/ufocatch";
-	private static final String DIR_BASE_PDF = "tmp/ufocatch/pdf";
 	
 	public static String getPath(String filename) {
 		{
@@ -46,54 +39,7 @@ public class Atom {
 		logger.error("Unexpected filename {}", filename);
 		throw new UnexpectedException("Unexpected filename");
 	}
-	
-	private static final Pattern PAT_TDID = Pattern.compile(".+?\\/(TD[0-9]{13})$");
-	public static String getTDID(String url) {
-		Matcher m = PAT_TDID.matcher(url);
-		if (m.matches()) {
-			String tdid = m.group(1);
-			return tdid;
-		} else {
-			logger.error("Unexpected url {}", url);
-			throw new UnexpectedException("Unexpected url");
-		}
-	}
-	public static String getPathPDF(String tdid) {
-		return String.format("%s/%s.pdf", DIR_BASE_PDF, tdid);
-	}
-	public static byte[] downloadPDFRawData(String tdid) {
-		String referer = String.format("https://resource.ufocatch.com/pdfpage/%s", tdid);
-		String url = String.format("https://resource.ufocatch.com/pdf/tdnet/%s", tdid);
 		
-		HttpUtil.Result result = HttpUtil.getInstance().withReferer(referer).withRawData(true).download(url);
-		return result.rawData;
-	}
-	private static List<File> fileListPDF = null;
-	public static List<File> getFileListPDF() {
-		if (fileListPDF == null) {
-			fileListPDF = FileUtil.listFile(new File(DIR_BASE_PDF)).stream().filter(o -> o.getName().endsWith(".pdf")).collect(Collectors.toList());
-			Collections.sort(fileListPDF);
-		}
-		return fileListPDF;
-	}
-	private static List<File> fileListUniqueuePDF = null;
-	public static List<File> getFileListUniqueuePDF() {
-		if (fileListUniqueuePDF == null) {
-			Set<String> md5Set = new TreeSet<>();
-			for(File file: FileUtil.listFile(new File(DIR_BASE_PDF)).stream().filter(o -> o.getName().endsWith(".pdf")).collect(Collectors.toList())) {
-				String md5 = StringUtil.toHexString(HashCode.getHashCode(file));
-				if (md5Set.contains(md5)) {
-					// same file content is appeared as other file name.
-				} else {
-					md5Set.add(md5);
-					fileListUniqueuePDF.add(file);
-				}
-			}
-			Collections.sort(fileListUniqueuePDF);
-		}
-		return fileListUniqueuePDF;
-	}	
-	
 	private static List<File> fileList = null;
 	public static List<File> getFileList() {
 		if (fileList == null) {
@@ -186,4 +132,39 @@ public class Atom {
 		Feed ret = JAXB.unmarshal(new StringReader(result.result), Feed.class);
 		return ret;
 	}
+	
+	public static byte[] downloadPDF(String tdid) {
+		String referer = String.format("https://resource.ufocatch.com/pdfpage/%s", tdid);
+		String target  = String.format("https://resource.ufocatch.com/pdf/tdnet/%s", tdid);
+		
+		HttpUtil.Result result = HttpUtil.getInstance().withReferer(referer).withRawData(true).download(target);
+		return result.rawData;
+	}
+
+	private static final Pattern PAT_TDID = Pattern.compile(".+/(TD[0-9]{13})$");
+	public static String getTDIDFromURL(String url) {
+		Matcher m = PAT_TDID.matcher(url);
+		if (m.matches()) {
+			String tdid = m.group(1);
+			return tdid;
+		} else {
+			logger.error("Unexpected url {}", url);
+			throw new UnexpectedException("Unexpected url");
+		}
+	}
+	private static final Pattern PAT_STOCK_CODE = Pattern.compile("【([0-9]{5})】.+");
+	public static String getStockCodeFromTitle(String title) {
+		Matcher m = PAT_STOCK_CODE.matcher(title);
+		if (m.matches()) {
+			String stockCode = m.group(1);
+			if (stockCode.endsWith("0")) {
+				stockCode = stockCode.substring(0, stockCode.length() - 1);
+			}
+			return stockCode;
+		} else {
+			logger.error("Unexpected title {}", title);
+			throw new UnexpectedException("Unexpected title");
+		}
+	}
+	
 }
