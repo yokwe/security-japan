24c68c20c4a83a1c1dc020af1804e8818ab86b16
hasegawa <hasegawa@ubuntudev.local>
2020-02-18 14:38:33 +0900

    Merge data from DividendETF.load().

--- src/yokwe/security/japan/data/UpdateDividend.java
+++ src/yokwe/security/japan/data/UpdateDividend.java
@@ -3,9 +3,7 @@ package yokwe.security.japan.data;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 import org.slf4j.LoggerFactory;
 
@@ -16,9 +14,6 @@ public class UpdateDividend {
 	
 	public static void main(String[] args) {
 		logger.info("START");
-		
-		Set<String> stockCodeSet = Stock.load().stream().map(o -> o.stockCode).collect(Collectors.toSet());
-		logger.info("stockCodeSet {}", stockCodeSet.size());
 		
 		Map<String, List<Dividend>> map = new TreeMap<>();
 		{
@@ -53,27 +48,50 @@ public class UpdateDividend {
 				dividendList.add(new Dividend(e.date, e.stockCode, e.distribution + e.distributionExcess));
 			}
 		}
-		
-		logger.info("map {}", map.size());
-		List<String> delist = new ArrayList<>();
-		int countSave  = 0;
-		int countDelist = 0;
-		for(Map.Entry<String, List<Dividend>> entry: map.entrySet()) {
-			String         stockCode = entry.getKey();
-			List<Dividend> list      = entry.getValue();
-			
-			// Save dividend of stockCode that appeared in stockCodeSet
-			if (stockCodeSet.contains(stockCode)) {
-				Dividend.save(stockCode, list);
-				countSave++;
-			} else {
-				delist.add(stockCode);
-				countDelist++;
+		{
+			List<DividendETF> list = DividendETF.load();
+			logger.info("DividendETF {}", list.size());
+			for(DividendETF e: list) {
+				if (!e.currency.equals("JPY")) continue;
+				
+				String stockCode = e.stockCode;
+				
+				List<Dividend> dividendList;
+				if (map.containsKey(stockCode)) {
+					dividendList = map.get(stockCode);
+				} else {
+					dividendList = new ArrayList<>();
+					map.put(stockCode, dividendList);
+				}
+				dividendList.add(new Dividend(e.date, e.stockCode, (e.dividend / e.unit)));
 			}
 		}
-		logger.info("countSave  {}", countSave);
-		logger.info("countDelist {}", countDelist);
-		logger.info("delist {} {}", delist.size(), delist);
+		{
+			Map<String, Stock> stockMap = Stock.getMap();
+			
+			logger.info("stockMap {}", stockMap.size());
+			
+			logger.info("map {}", map.size());
+			List<String> delist = new ArrayList<>();
+			int countSave   = 0;
+			int countDelist = 0;
+			for(Map.Entry<String, List<Dividend>> entry: map.entrySet()) {
+				String         stockCode = entry.getKey();
+				List<Dividend> list      = entry.getValue();
+				
+				// Save dividend of stockCode that appeared in stockCodeSet
+				if (stockMap.containsKey(stockCode)) {
+					Dividend.save(stockCode, list);
+					countSave++;
+				} else {
+					delist.add(stockCode);
+					countDelist++;
+				}
+			}
+			logger.info("countSave  {}", countSave);
+			logger.info("countDelist {}", countDelist);
+			logger.info("delist {} {}", delist.size(), delist);
+		}
 		
 		logger.info("STOP");
 	}
