18e0232e811d5e5b3e2eca1feca8826bae440b9c
hasegawa <hasegawa@ubuntudev.local>
2020-02-20 14:28:21 +0900

    Add more general report of stock and reit.

---
+++ src/yokwe/security/japan/xbrl/report/REITReport.java
@@ -0,0 +1,87 @@
+package yokwe.security.japan.xbrl.report;
+
+import static yokwe.security.japan.xbrl.inline.Context.CURRENT_YEAR_DURATION;
+import static yokwe.security.japan.xbrl.inline.Context.RESULT_MEMBER;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_RE_T_LABEL.DISTRIBUTIONS_IN_EXCESS_OF_PROFIT_PER_UNIT_REIT;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_RE_T_LABEL.DISTRIBUTIONS_PAYABLE_DATE_AS_PLANNED_REIT;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_RE_T_LABEL.DISTRIBUTIONS_PER_UNIT_EXCLUDING_DISTRIBUTIONS_IN_EXCESS_OF_PROFIT_REIT;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_RE_T_LABEL.FILING_DATE;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_RE_T_LABEL.FISCAL_YEAR_END;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_RE_T_LABEL.ISSUER_NAME_REIT;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_RE_T_LABEL.PAYOUT_RATIO;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_RE_T_LABEL.SECURITIES_CODE;
+
+import java.math.BigDecimal;
+
+import org.slf4j.LoggerFactory;
+
+import yokwe.security.japan.xbrl.inline.Document;
+import yokwe.util.CSVUtil.ColumnName;
+
+public class REITReport extends AbstractReport implements Comparable<REITReport> {
+	static final org.slf4j.Logger logger = LoggerFactory.getLogger(REITReport.class);
+
+	@TSE_RE(label = FILING_DATE)
+	@ColumnName("提出日")
+	public String filingDate;
+	
+	@TSE_RE(label = ISSUER_NAME_REIT)
+	@ColumnName("発行者名")
+	public String company;
+	
+	@TSE_RE(label = SECURITIES_CODE)
+	@ColumnName("コード番号")
+	public String stockCode;
+	
+	@TSE_RE(label = FISCAL_YEAR_END)
+	@ColumnName("決算期")
+	public String yearEnd;
+	
+	// DIVIDEND
+	@TSE_RE(label = DISTRIBUTIONS_PAYABLE_DATE_AS_PLANNED_REIT,
+			acceptNullOrEmpty = true)
+	@ColumnName("分配金支日")
+	public String distributionsDate;
+	
+	@TSE_RE(label = DISTRIBUTIONS_PER_UNIT_EXCLUDING_DISTRIBUTIONS_IN_EXCESS_OF_PROFIT_REIT,
+			contextIncludeAll = {CURRENT_YEAR_DURATION, RESULT_MEMBER},
+			acceptNullOrEmpty = true)
+	@ColumnName("分配金")
+	public BigDecimal distributionsPerUnit;
+
+	@TSE_RE(label = DISTRIBUTIONS_IN_EXCESS_OF_PROFIT_PER_UNIT_REIT,
+			contextIncludeAll = {CURRENT_YEAR_DURATION, RESULT_MEMBER},
+			acceptNullOrEmpty = true)
+	@ColumnName("利益超過分配金")
+	public BigDecimal distributionsInExcessOfProfitPerUnit;
+
+	@TSE_RE(label = PAYOUT_RATIO,
+			contextIncludeAll = {CURRENT_YEAR_DURATION, RESULT_MEMBER},
+			acceptNullOrEmpty = true)
+	@ColumnName("配当性向")
+	public BigDecimal payoutRatio;
+	
+	public String filename;
+
+	
+	public static REITReport getInstance(Document document) {
+		REITReport ret = AbstractReport.getInstance(REITReport.class, document);
+		ret.filename = document.filename;
+		
+		return ret;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("{%s %s %s %s %s %s}",  stockCode, yearEnd, filingDate, distributionsDate, distributionsPerUnit, distributionsInExcessOfProfitPerUnit);
+	}
+
+	// Define natural ordering of DividendBriefReport
+	@Override
+	public int compareTo(REITReport that) {
+		int ret = this.stockCode.compareTo(that.stockCode);
+		if (ret == 0) ret = this.yearEnd.compareTo(that.yearEnd);
+		if (ret == 0) ret = this.filingDate.compareTo(that.filingDate);
+		return ret;
+	}
+}
---
+++ src/yokwe/security/japan/xbrl/report/StockReport.java
@@ -0,0 +1,174 @@
+package yokwe.security.japan.xbrl.report;
+
+import static yokwe.security.japan.xbrl.inline.Context.ANNUAL_MEMBER;
+import static yokwe.security.japan.xbrl.inline.Context.CURRENT_YEAR_DURATION;
+import static yokwe.security.japan.xbrl.inline.Context.FIRST_QUARTER_MEMBER;
+import static yokwe.security.japan.xbrl.inline.Context.FORECAST_MEMBER;
+import static yokwe.security.japan.xbrl.inline.Context.LOWER_MEMBER;
+import static yokwe.security.japan.xbrl.inline.Context.RESULT_MEMBER;
+import static yokwe.security.japan.xbrl.inline.Context.SECOND_QUARTER_MEMBER;
+import static yokwe.security.japan.xbrl.inline.Context.THIRD_QUARTER_MEMBER;
+import static yokwe.security.japan.xbrl.inline.Context.UPPER_MEMBER;
+import static yokwe.security.japan.xbrl.inline.Context.YEAR_END_MEMBER;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_ED_T_LABEL.ANNUAL_SECURITIES_REPORT_FILING_DATE_AS_PLANNED;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_ED_T_LABEL.COMPANY_NAME;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_ED_T_LABEL.DATE_OF_GENERAL_SHAREHOLDERS_MEETING_AS_PLANNED;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_ED_T_LABEL.DIVIDEND_PAYABLE_DATE_AS_PLANNED;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_ED_T_LABEL.DIVIDEND_PER_SHARE;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_ED_T_LABEL.FILING_DATE;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_ED_T_LABEL.FISCAL_YEAR_END;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_ED_T_LABEL.QUARTERLY_PERIOD;
+import static yokwe.security.japan.xbrl.taxonomy.TSE_ED_T_LABEL.SECURITIES_CODE;
+
+import java.math.BigDecimal;
+
+import org.slf4j.LoggerFactory;
+
+import yokwe.UnexpectedException;
+import yokwe.security.japan.xbrl.inline.Document;
+import yokwe.util.CSVUtil.ColumnName;
+
+public class StockReport extends AbstractReport implements Comparable<StockReport> {
+	static final org.slf4j.Logger logger = LoggerFactory.getLogger(StockReport.class);
+
+	@TSE_ED(label = FILING_DATE,
+			acceptNullOrEmpty = true)
+	@ColumnName("提出日")
+	public String filingDate;
+	
+	@TSE_ED(label = COMPANY_NAME)
+	@ColumnName("会社名")
+	public String companyName;
+	
+	@TSE_ED(label = SECURITIES_CODE)
+	@ColumnName("コード番号")
+	public String securitiesCode;
+	
+	@TSE_ED(label = FISCAL_YEAR_END)
+	@ColumnName("決算期")
+	public String fiscalYearEnd;
+	
+	@TSE_ED(label = QUARTERLY_PERIOD,
+			acceptNullOrEmpty = true)
+	@ColumnName("四半期")
+	public Integer quarterlyPeriod;
+	
+	// DATE_OF_GENERAL_SHAREHOLDERS_MEETING_AS_PLANNED
+	@TSE_ED(label = DATE_OF_GENERAL_SHAREHOLDERS_MEETING_AS_PLANNED,
+			acceptNullOrEmpty = true)
+	@ColumnName("株主総会")
+	public String dateOfGeneralShareholdersMeetingAsPlanned;
+
+	@TSE_ED(label = ANNUAL_SECURITIES_REPORT_FILING_DATE_AS_PLANNED,
+			acceptNullOrEmpty = true)
+	@ColumnName("有価証券報告書")
+	public String annualSecuritiesReportFilingDateAsPlanned;
+	
+	@TSE_ED(label = DIVIDEND_PAYABLE_DATE_AS_PLANNED,
+			acceptNullOrEmpty = true)
+	@ColumnName("配当支払日")
+	public String dividendPayableDateAsPlanned;
+
+	@TSE_ED(label = DIVIDEND_PER_SHARE,
+			contextIncludeAll = {CURRENT_YEAR_DURATION, FIRST_QUARTER_MEMBER},
+			contextExcludeAny = {LOWER_MEMBER, UPPER_MEMBER},
+			acceptNullOrEmpty = true)
+	@ColumnName("配当Q1")
+	public BigDecimal dividendPerShareQ1; // PriorYearDuration/CurrentYearDuration FirstQuarterMember/SecondQuarterMember/ThirdQuarterMember/YearEndMember/AnnualMember
+
+	@TSE_ED(label = DIVIDEND_PER_SHARE,
+			contextIncludeAll = {CURRENT_YEAR_DURATION, SECOND_QUARTER_MEMBER},
+			contextExcludeAny = {LOWER_MEMBER, UPPER_MEMBER},
+			acceptNullOrEmpty = true)
+	@ColumnName("配当Q2")
+	public BigDecimal dividendPerShareQ2; // PriorYearDuration/CurrentYearDuration FirstQuarterMember/SecondQuarterMember/ThirdQuarterMember/YearEndMember/AnnualMember
+
+	@TSE_ED(label = DIVIDEND_PER_SHARE,
+			contextIncludeAll = {CURRENT_YEAR_DURATION, THIRD_QUARTER_MEMBER},
+			contextExcludeAny = {LOWER_MEMBER, UPPER_MEMBER},
+			acceptNullOrEmpty = true)
+	@ColumnName("配当Q3")
+	public BigDecimal dividendPerShareQ3; // PriorYearDuration/CurrentYearDuration FirstQuarterMember/SecondQuarterMember/ThirdQuarterMember/YearEndMember/AnnualMember
+
+	@TSE_ED(label = DIVIDEND_PER_SHARE,
+			contextIncludeAll = {CURRENT_YEAR_DURATION, YEAR_END_MEMBER},
+			contextExcludeAny = {LOWER_MEMBER, UPPER_MEMBER},
+			acceptNullOrEmpty = true)
+	@ColumnName("配当Q4")
+	public BigDecimal dividendPerShareQ4; // PriorYearDuration/CurrentYearDuration FirstQuarterMember/SecondQuarterMember/ThirdQuarterMember/YearEndMember/AnnualMember
+
+	@ColumnName("配当")
+	public BigDecimal dividendPerShare;
+
+	@TSE_ED(label = DIVIDEND_PER_SHARE,
+			contextIncludeAll = {CURRENT_YEAR_DURATION, ANNUAL_MEMBER, FORECAST_MEMBER},
+			contextExcludeAny = {LOWER_MEMBER, UPPER_MEMBER},
+			acceptNullOrEmpty = true)
+	@ColumnName("配当年間予想")
+	public BigDecimal annualDividendPerShareForeast; // PriorYearDuration/CurrentYearDuration FirstQuarterMember/SecondQuarterMember/ThirdQuarterMember/YearEndMember/AnnualMember
+
+	// CurrentYearDuration_AnnualMember_NonConsolidatedMember_ResultMember
+	@TSE_ED(label = DIVIDEND_PER_SHARE,
+			contextIncludeAll = {CURRENT_YEAR_DURATION, ANNUAL_MEMBER, RESULT_MEMBER},
+			contextExcludeAny = {LOWER_MEMBER, UPPER_MEMBER},
+			acceptNullOrEmpty = true)
+	@ColumnName("配当年間実績")
+	public BigDecimal annualDividendPerShareResult; // PriorYearDuration/CurrentYearDuration FirstQuarterMember/SecondQuarterMember/ThirdQuarterMember/YearEndMember/AnnualMember	
+
+	@ColumnName("配当年間")
+	public BigDecimal annualDividendPerShare;
+	
+	public String filename;
+
+	public static StockReport getInstance(Document document) {
+		StockReport ret = AbstractReport.getInstance(StockReport.class, document);
+		ret.filename = document.filename;
+
+		switch(ret.quarterlyPeriod) {
+		case 0:
+			ret.quarterlyPeriod        = 4;
+			ret.dividendPerShare       = ret.dividendPerShareQ4;
+			ret.annualDividendPerShare = ret.annualDividendPerShareResult;
+			break;
+		case 1:
+			ret.dividendPerShare       = ret.dividendPerShareQ1;
+			ret.annualDividendPerShare = ret.annualDividendPerShareForeast;
+			break;
+		case 2:
+			ret.dividendPerShare       = ret.dividendPerShareQ2;
+			ret.annualDividendPerShare = ret.annualDividendPerShareForeast;
+			break;
+		case 3:
+			ret.dividendPerShare       = ret.dividendPerShareQ3;
+			ret.annualDividendPerShare = ret.annualDividendPerShareForeast;
+			break;
+		default:
+			logger.error("Unexpected quarterlyPeriod {}", ret.quarterlyPeriod);
+			throw new UnexpectedException("Unexpected quarterlyPeriod");
+		}
+
+		// Sanity check
+		if (ret.filingDate.isEmpty()) {
+			logger.warn("filingDate is empty  {}", ret);
+		}
+
+		return ret;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s %s %s %d %s %s %s %s",
+			securitiesCode, filingDate, fiscalYearEnd, quarterlyPeriod, annualSecuritiesReportFilingDateAsPlanned,
+			annualDividendPerShareForeast, annualDividendPerShareResult, companyName);
+	}
+
+	// Define natural ordering of DividendBriefReport
+	@Override
+	public int compareTo(StockReport that) {
+		int ret = this.securitiesCode.compareTo(that.securitiesCode);
+		if (ret == 0) ret = this.fiscalYearEnd.compareTo(that.fiscalYearEnd);
+		if (ret == 0) ret = this.quarterlyPeriod - that.quarterlyPeriod;
+		if (ret == 0) ret = this.filingDate.compareTo(that.filingDate);
+		return ret;
+	}
+}
