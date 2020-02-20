bfb807b6ad8efcd1895f8b3c76b0bdba20cfed4f
hasegawa <hasegawa@ubuntudev.local>
2020-02-20 14:25:46 +0900

    Document.getInstance() takes File as parameter. Remove unmodifiedXXX methods from Collections.

--- src/yokwe/security/japan/xbrl/inline/Document.java
+++ src/yokwe/security/japan/xbrl/inline/Document.java
@@ -1,5 +1,6 @@
 package yokwe.security.japan.xbrl.inline;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -9,18 +10,20 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.stream.Stream;
 
+import yokwe.util.XMLUtil;
 import yokwe.util.XMLUtil.QValue;
-import yokwe.util.XMLUtil.XMLElement;
 
 public class Document {
 	private static final List<InlineXBRL> EMPTY_LIST = Collections.unmodifiableList(new ArrayList<>());
 
+	public  final String                        filename;
 	private final List<InlineXBRL>              all;
 	private final Map<QValue, List<InlineXBRL>> map;
 	
-	private Document(List<InlineXBRL> all, Map<QValue, List<InlineXBRL>> map) {
-		this.all = all;
-		this.map = map;
+	private Document(String filename, List<InlineXBRL> all, Map<QValue, List<InlineXBRL>> map) {
+		this.filename = filename;
+		this.all      = all;
+		this.map      = map;
 	}
 
 	private static void buildMap(List<InlineXBRL> all, Map<QValue, List<InlineXBRL>> map, InlineXBRL ix) {
@@ -38,21 +41,13 @@ public class Document {
 		list.add(ix);
 	}
 	
-	public static Document getInstance(Stream<XMLElement> stream) {
+	public static Document getInstance(File file) {
 		List<InlineXBRL>              all = new ArrayList<>();
 		Map<QValue, List<InlineXBRL>> map = new TreeMap<>();
 		
-		stream.filter(InlineXBRL::canGetInstance).forEach(o -> buildMap(all, map, InlineXBRL.getInstance(o)));
+		XMLUtil.buildStream(file).filter(InlineXBRL::canGetInstance).forEach(o -> buildMap(all, map, InlineXBRL.getInstance(o)));
 		
-		// Make unmodifiable element of map
-		Map<QValue, List<InlineXBRL>> map2 = new TreeMap<>();
-		for(Map.Entry<QValue, List<InlineXBRL>> entry: map.entrySet()) {
-			QValue key = entry.getKey();
-			List<InlineXBRL> value = entry.getValue();
-			map2.put(key, Collections.unmodifiableList(value));
-		}
-
-		return new Document(Collections.unmodifiableList(all), Collections.unmodifiableMap(map2));
+		return new Document(file.getName(), all, map);
 	}
 
 	public List<InlineXBRL> getList() {
