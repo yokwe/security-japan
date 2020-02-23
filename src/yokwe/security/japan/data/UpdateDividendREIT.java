13fd798e23915f6a9a2d1d5c823e1e84d92759a0
hasegawa <hasegawa@ubuntudev.local>
2020-02-23 16:02:48 +0900

    Create dividend-reit.csv from reit-report.csv

---
+++ src/yokwe/security/japan/data/UpdateDividendREIT.java
@@ -0,0 +1,129 @@
+package yokwe.security.japan.data;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.slf4j.LoggerFactory;
+
+import yokwe.security.japan.jpx.tdnet.Period;
+import yokwe.security.japan.jpx.tdnet.SummaryFilename;
+import yokwe.security.japan.ufocatch.Atom;
+import yokwe.security.japan.xbrl.inline.Document;
+import yokwe.security.japan.xbrl.report.REITReport;
+
+public class UpdateDividendREIT {
+	static final org.slf4j.Logger logger = LoggerFactory.getLogger(UpdateDividendREIT.class);
+	
+	public static void main(String[] args) {
+		logger.info("START");
+		
+		Map<String, DividendREIT> map = new TreeMap<>();
+		
+		{
+			Map<SummaryFilename, REITReport> reportMap = REITReport.getMap();
+			logger.info("reportMap {}", reportMap.size());
+			
+			Map<SummaryFilename, File> fileMap = new TreeMap<>();
+			{
+				for(Map.Entry<SummaryFilename, File> entry: Atom.getFileMap().entrySet()) {
+					SummaryFilename key   = entry.getKey();
+					File            value = entry.getValue();
+					switch(key.category) {
+					case REJP:
+						if (key.period == Period.ANNUAL) {
+							fileMap.put(key, value);
+						}
+						break;
+					default:
+						break;
+					}
+				}
+
+			}
+			logger.info("fileMap {}", fileMap.size());
+			
+			
+			int count = 0;
+			for(Map.Entry<SummaryFilename, File> entry: fileMap.entrySet()) {
+				SummaryFilename key  = entry.getKey();
+				File            file = entry.getValue();
+				if ((count % 1000) == 0) {
+					logger.info("{} {}", String.format("%5d / %5d", count, fileMap.size()), key);
+				}
+				count++;
+				
+				final String  date;
+				final String  stockCode;
+				final Double  dividend;
+				
+				final String  yearEnd;
+				final Integer quarter;
+				
+				final SummaryFilename filename;
+
+				{
+					final REITReport value;
+					if (reportMap.containsKey(key)) {
+						value = reportMap.get(key);
+					} else {
+						Document document = Document.getInstance(file);
+						value = REITReport.getInstance(document);
+					}
+					
+					date      = value.distributionsDate;
+					stockCode = value.stockCode;
+					dividend  = value.distributionsPerUnit.doubleValue() + value.distributionsInExcessOfProfitPerUnit.doubleValue();
+					
+					yearEnd   = value.yearEnd;
+					quarter   = 4;
+					
+					filename = value.filename;
+				}
+								
+				String        mapKey   = String.format("%s %s %s", stockCode, yearEnd, quarter);
+				DividendREIT mapValue = new DividendREIT(stockCode, yearEnd, quarter, date, dividend, filename);
+
+				// Sanity check
+				{
+					if (date.isEmpty()) {
+//						logger.warn("date is null {}", mapValue);
+						continue;
+					}
+					if (stockCode.isEmpty()) {
+						logger.warn("stockCode is null {}", mapValue);
+						continue;
+					}
+					if (yearEnd.isEmpty()) {
+						logger.warn("yearEnd is null {}", mapValue);
+						continue;
+					}
+				}
+
+				if (map.containsKey(mapKey)) {
+					DividendREIT oldValue = map.get(mapKey);
+					if (mapValue.equals(oldValue)) continue;
+					
+					logger.warn("====");
+					logger.warn("Overwrite existing {}", key);
+					logger.warn("  old {}", oldValue);
+					logger.warn("  new {}", mapValue);
+				} else {
+					map.put(mapKey, mapValue);
+				}
+			}
+		}
+		
+		logger.info("map {}", map.size());
+		{
+			List<DividendREIT> dividendList = new ArrayList<>(map.values());
+			
+			logger.info("save {}  {}", DividendREIT.PATH_FILE, dividendList.size());
+			DividendREIT.save(dividendList);
+		}
+
+		logger.info("STOP");
+	}
+}
