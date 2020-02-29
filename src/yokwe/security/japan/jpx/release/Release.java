b0ef04c1750496717d7e3a8a81c2c2da4a7af314
hasegawa <hasegawa@ubuntudev.local>
2020-02-29 15:26:51 +0900

    UpdateRelase works as expected

--- src/yokwe/security/japan/jpx/release/Release.java
+++ src/yokwe/security/japan/jpx/release/Release.java
@@ -1,190 +1,195 @@
 package yokwe.security.japan.jpx.release;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import org.slf4j.LoggerFactory;
 
 import yokwe.UnexpectedException;
+import yokwe.util.CSVUtil;
+import yokwe.util.FileUtil;
 import yokwe.util.HttpUtil;
-import yokwe.util.StringUtil;
 
-public class Release {
+public class Release implements Comparable<Release> {
 	static final org.slf4j.Logger logger = LoggerFactory.getLogger(Release.class);
-
-	public static final String PATH_DIR_ZIP = "tmp/tdnet/zip";
-	public static final String PATH_DIR_PDF = "tmp/tdnet/pdf";
 	
-	public static final String URL_MAIN = "https://www.release.tdnet.info/inbs/I_main_00.html";
-	
-	private static final HttpUtil httpUtil = HttpUtil.getInstance();
-
-	public static class Entry {
-		public String time;
-		public String code;   // FIXME not stockCode
-		public String name;
-		public String pdf;
-		public String title;
-		public String xbrl;
-		public String place;
-		public String history;
-		
-		public Entry(String time, String code, String name, String pdf, String title, String xbrl, String place, String history) {
-			this.time    = time;
-			this.code    = code;
-			this.name    = name;
-			this.pdf     = pdf;
-			this.title   = title;
-			this.xbrl    = xbrl;
-			this.place   = place;
-			this.history = history;
-		}
-		@Override
-		public String toString() {
-			return String.format("{%s %s %s %s %s %s %s %s}", time, code, name, pdf, title, xbrl, place, history);
-		}
+	public static final String URL_BASE = "https://www.release.tdnet.info/inbs";
+	public static String getPageURL(LocalDate date, int page) {
+		String dateString = String.format("%4d%02d%02d", date.getYear(), date.getMonthValue(), date.getDayOfMonth());
+		return String.format("%s/I_list_%03d_%s.html", URL_BASE, page, dateString);
+	}
+	public static String getDataURL(String filename) {
+		return String.format("%s/%s", URL_BASE, filename);
+	}
+	public static byte[] downloadData(String filename) {
+		String url = getDataURL(filename);
+		HttpUtil.Result result = HttpUtil.getInstance().withRawData(true).download(url);
+		return result.rawData;
 	}
 	
-	public static String[] getGroup(Pattern pat, String string) {
-		Matcher m = pat.matcher(string);
-		if (m.find()) {
-			int count = m.groupCount();
-			String[] ret = new String[count];
-			for(int i = 0; i < count; i++) {
-				ret[i] = m.group(i + 1);
-			}
-			return ret;
-		} else {
-			return null;
+	private static List<File> dataFileList = null;
+	public static List<File> getDataFileList() {
+		if (dataFileList == null) {
+			dataFileList = FileUtil.listFile(PATH_DATA_DIR).stream().
+					filter(o -> o.getName().endsWith(".pdf") || o.getName().endsWith(".zip")).
+					collect(Collectors.toList());
 		}
-	}
-	public static String getGroupOne(Pattern pat, String string) {
-		String[] result = getGroup(pat, string);
-		if (result == null) {
-			return null;
-		} else {
-			if (result.length == 1) {
-				return result[0];
-			} else {
-				logger.error("result.length != 1");
-				logger.error("  result {}", Arrays.asList(result));
-				throw new UnexpectedException("result.length != 1");
-			}
-		}
+		return dataFileList;
 	}
 	
-	public static class Page {
-		public static final String URL_BASE = "https://www.release.tdnet.info/inbs";
-		public static String getURL(LocalDate date, int page) {
-			String dateString = String.format("%4d%02d%02d", date.getYear(), date.getMonthValue(), date.getDayOfMonth());
-			return String.format("%s/I_list_%03d_%s.html", URL_BASE, page, dateString);
-		}
-		public static byte[] downloadDataFile(String filename) {
-			String url = String.format("%s/%s", URL_BASE, filename);
-			HttpUtil.Result result = HttpUtil.getInstance().withRawData(true).download(url);
-			return result.rawData;
-		}
-		
-		// <div id="kaiji-date-1">2020年01月27日</div>
-		private static final Pattern KAIJI_DATE_PAT = Pattern.compile("<div id=\"kaiji-date-1\">(.+?)</div>");
 
-		// 2020年02月27日
-		private static final Pattern DATE_PAT = Pattern.compile("(20[0-9][0-9])年([01][0-9])月([0-3][0-9])日");
-		private static StringUtil.MatcherFunction<LocalDate> DATE_OP = (m -> LocalDate.parse(String.format("%s-%s-%s", m.group(1), m.group(2), m.group(3))));
-		
-		// <div id="kaiji-text-1">に開示された情報はありません。</div>
-		private static final Pattern KAIJI_TEXT_PAT = Pattern.compile("<div id=\"kaiji-text-1\">(.+?)</div>");
-		private static final String KAIJI_TEXT_NO_DATA = "に開示された情報はありません。";
-		
-		// <div class="pager-R" onClick="pager('I_list_002_20200127.html')"><span>次へ</span><img src="./gif/icn_next.png" alt="次へ"></img></div>
-		// <div class="pager-R" onClick="pager('')"><span>次へ</span><img src="./gif/icn_next.png" alt="次へ"></img></div>
-		private static final Pattern NEXT_PAT = Pattern.compile("<div class=\"pager-R\" onClick=\"pager\\('(.*?)'\\)\">");
-		
-		private static final Pattern ENTRY_PAT = Pattern.compile(
-				"<tr.*?>\\s+" +
-				"<td class=\"(?:even|odd)new-L kjTime\".+?>(.+?)</td>\\s+" +
-				"<td class=\"(?:even|odd)new-M kjCode\".+?>(.+?)</td>\\s+" +
-				"<td class=\"(?:even|odd)new-M kjName\".+?>(.+?)\\s*</td>\\s+" +
-				"<td class=\"(?:even|odd)new-M kjTitle\".+?><a href=\"(.+?)\" .+?>(.+?)</a>(?:<BR>.+?)?</td>\\s+" +
-				"<td class=\"(?:even|odd)new-M kjXbrl\".+?>(?:<div class=\"xbrl-mask\">.+href=\"(.+?)\".+?)? </td>\\s+" +
-				"<td class=\"(?:even|odd)new-M kjPlace\".+?>(.+?)\\s*</td>\\s+" +
-				"<td class=\"(?:even|odd)new-R kjHistroy\".+?>(.*?)(?:[　]+?)?</td>\\s+" +
-				"</tr>"
-				);
-		private static StringUtil.MatcherFunction<Entry> ENTRY_OP = (m -> new Entry(m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6), m.group(7), m.group(8)));
+	public static final String PATH_DATA_DIR = "tmp/release";
+	public static File getDataFile(String filename) {
+		String yyyy = filename.substring(0, 4);
+		String path = String.format("%s/%s/%s", PATH_DATA_DIR, yyyy, filename);
+		return new File(path);
+	}
 
-		public LocalDate   date;
-		public List<Entry> entryList;
-		
-		private Page(LocalDate date) {
-			this.date      = date;
-			this.entryList = new ArrayList<>();
+	public static final String PATH_FILE = "tmp/data/release.csv";
+	
+	private static List<Release> list = null;
+	public static List<Release> getList() {
+		if (list == null) {
+			list = CSVUtil.read(Release.class).file(PATH_FILE);
 		}
-		@Override
-		public String toString() {
-			return String.format("{%s %3d}", date, entryList.size());
-		}
-		
-		public static Page getInstance(LocalDate date) {
-			Page ret = new Page(date);
+		return list;
+	}
+	private static Map<String, Release> map = null;
+	// key is id
+	public static Map<String, Release> getMap() {
+		if (map == null) {
+			map = new TreeMap<>();
+			List<Release> list = getList();
+			if (list == null) return map;
 			
-			for(int page = 1;; page++) {
-				String url = getURL(date, page);
-				logger.info("url {}", url);
-				HttpUtil.Result result = httpUtil.download(url);
-				String string = result.result;
-				if (string == null) break;
-				
-				{
-					String kaijiDate = getGroupOne(KAIJI_DATE_PAT, string);
-//					logger.info("kaijiDate {}", kaijiDate);
-					
-					List<LocalDate> list = StringUtil.find(kaijiDate, DATE_PAT, DATE_OP).collect(Collectors.toList());
-					if (!date.equals(list.get(0))) {
-						logger.error("date mismatch");
-						logger.error("  date {}", date);
-						logger.error("  list {}", list.get(0));
-						throw new UnexpectedException("date mismatch");
-					}
-				}
-				
-				{
-					String kaijiText = getGroupOne(KAIJI_TEXT_PAT, string);
-//					logger.info("kaijiText {}", kaijiText);
-					if (kaijiText.equals(KAIJI_TEXT_NO_DATA)) break;
-				}
-				
-				{
-					List<Entry> list = StringUtil.find(string, ENTRY_PAT, ENTRY_OP).collect(Collectors.toList());
-					logger.info("list {}", list.size());
-					ret.entryList.addAll(list);
-				}
-				
-				{
-					String next = getGroupOne(NEXT_PAT, string);
-					if (next.isEmpty()) break;
+			for(Release e: list) {
+				String key = e.id;
+				if (map.containsKey(key)) {
+					logger.error("Duplicate key {}", key);
+					logger.error("  old {}", map.get(key));
+					logger.error("  new {}", e);
+					throw new UnexpectedException("Duplicate key");
+				} else {
+					map.put(key, e);
 				}
 			}
-			return ret;
+		}
+		return map;
+	}
+	public static void save(Collection<Release> collection) {
+		List<Release> list = new ArrayList<>(collection);
+		save(list);
+	}
+	public static void save(List<Release> list) {
+		// Sort before save
+		Collections.sort(list);
+		CSVUtil.write(Release.class).file(PATH_FILE, list);
+	}
+
+
+	public String id;
+	public String pdf;
+	public String xbrl;
+	public String title;
+
+	public String time;
+	public String code;   // FIXME not stockCode
+	public String name;
+	public String place;
+	public String history;
+	public String date;
+	
+	public Release() {
+		this.id      = null;
+		this.pdf     = null;
+		this.xbrl    = null;
+		this.title   = null;
+		
+		this.time    = null;
+		this.code    = null;
+		this.name    = null;
+		this.place   = null;
+		this.history = null;
+		this.date    = null;
+	}
+	
+	public Release(String time, String code, String name, String pdf, String title, String xbrl, String place, String history) {
+		// sanity check
+		{
+			if (!pdf.endsWith(".pdf")) {
+				logger.error("Unexpected pdf");
+				logger.error("  pdf {}", pdf);
+				throw new UnexpectedException("Unexpected pdf");
+			}
+			if (xbrl != null && !xbrl.endsWith(".zip")) {
+				logger.error("Unexpected xbrl");
+				logger.error("  xbrl {}", xbrl);
+				throw new UnexpectedException("Unexpected xbrl");
+			}
+		}
+		
+		String pdfID = pdf.replace(".pdf", "").substring(4);
+		// sanity check
+		{
+			if (xbrl != null) {
+				String zipID = xbrl.replace(".zip", "").substring(4);
+
+				if (!pdfID.equals(zipID)) {
+					logger.error("id mismatch");
+					logger.error("  pdfID {}", pdfID);
+					logger.error("  zipID {}", zipID);
+					throw new UnexpectedException("id mismatch");
+				}
+			}
+		}
+		
+		this.id      = pdfID;
+		this.pdf     = pdf;
+		this.xbrl    = (xbrl == null) ? "" : xbrl;
+		this.title   = title;
+
+		this.time    = time;
+		this.code    = code;
+		this.name    = name;
+		this.place   = place;
+		this.history = history;
+		
+	}
+	@Override
+	public String toString() {
+		return String.format("{%s %s %s %s %22s %s}", id, date, time, pdf, xbrl, title);
+//		return String.format("{%s %s %s %s %s %s %s %s %s}", id, pdf, xbrl, title, time, code, name, place, history);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == null) return false;
+		if (o instanceof Release) {
+			Release that = (Release)o;
+			return this.id.equals(that.id);
+		} else {
+			return false;
 		}
 	}
 	
-	public static void main(String[] args) {
-		logger.info("START");
+	@Override
+	public int compareTo(Release that) {
+		int ret = this.id.compareTo(that.id);
 		
-		{
-			Page page = Page.getInstance(LocalDate.parse("2020-02-25"));
-			logger.info("page {}", page);
-			for(Entry e: page.entryList) {
-				logger.info("  {} {} {} {}  {}", e.time, e.code, e.pdf, e.xbrl, e.title);
-			}
+		// sanity check
+		if (ret == 0) {
+			logger.error("Duplicate key");
+			logger.error("  this {}", this);
+			logger.error("  that {}", that);
+			throw new UnexpectedException("Duplicate key");
 		}
-		
-		logger.info("STOP");
+		return ret;
 	}
 }
---
+++ src/yokwe/security/japan/jpx/release/UpdateRelease.java
@@ -0,0 +1,68 @@
+package yokwe.security.japan.jpx.release;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.LoggerFactory;
+
+import yokwe.util.FileUtil;
+
+public class UpdateRelease {
+	static final org.slf4j.Logger logger = LoggerFactory.getLogger(UpdateRelease.class);
+	
+	public static void main(String[] args) {
+		logger.info("START");
+		
+		Map<String, Release> map = Release.getMap();
+		logger.info("map {}", map.size());
+
+		LocalDate date = LocalDate.now();
+		{
+			for(;;) {
+				Page page = Page.getInstance(date);
+				if (page == null) break;
+				logger.info("page {}", page);
+				for(Release e: page.entryList) {
+//					logger.info("  {}", e);
+					
+//					{
+//						String filename = String.format("%s.pdf", e.id);
+//						File file = Release.getDataFile(filename);
+//
+//						if (!file.exists()) {
+//							logger.info("file {}", file.getPath());
+//							byte[] content  = Release.downloadData(e.pdf);
+//							FileUtil.rawWrite().file(file, content);
+//						}
+//					}
+					{
+						if (!e.xbrl.isEmpty()) {
+							String filename = String.format("%s.zip", e.id);
+							File file = Release.getDataFile(filename);
+
+							if (!file.exists()) {
+								logger.info("file {}", file.getPath());
+								byte[] content  = Release.downloadData(e.xbrl);
+								FileUtil.rawWrite().file(file, content);
+							}
+						}
+					}
+
+					map.put(e.id, e);
+				}
+				date = date.minusDays(1);
+			}
+		}
+		
+		{
+			List<Release> list = new ArrayList<>(map.values());
+			logger.info("save {} {}", Release.PATH_FILE, list.size());
+			Release.save(list);
+		}
+		
+		logger.info("STOP");
+	}
+}
