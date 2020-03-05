3de82b0ea56177fb73859bbc300953c7f92d21eb
hasegawa <hasegawa@ubuntudev.local>
2020-03-05 20:56:07 +0900

    Add Document class.

--- src/yokwe/security/japan/edinet/API.java
+++ src/yokwe/security/japan/edinet/API.java
@@ -1,5 +1,6 @@
 package yokwe.security.japan.edinet;
 
+import java.nio.charset.Charset;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -7,6 +8,8 @@ import javax.json.JsonObject;
 
 import org.slf4j.LoggerFactory;
 
+import yokwe.UnexpectedException;
+import yokwe.util.HttpUtil;
 import yokwe.util.json.JSONBase;
 
 public class API {
@@ -14,7 +17,7 @@ public class API {
 
 	public static final String URL_BASE = "https://disclosure.edinet-fsa.go.jp/api";
 	
-	enum Version {
+	public static enum Version {
 		V1("v1");
 		
 		final String value;
@@ -29,6 +32,100 @@ public class API {
 		}
 	}
 	
+	public static enum DocumentType {
+		DOC_010("010", "有価証券通知書"),
+		DOC_020("020", "変更通知書(有価証券通知書)"),
+		DOC_030("030", "有価証券届出書"),
+		DOC_040("040", "訂正有価証券届出書"),
+		DOC_050("050", "届出の取下げ願い"),
+		DOC_060("060", "発行登録通知書"),
+		DOC_070("070", "変更通知書(発行登録通知書)"),
+		DOC_080("080", "発行登録書"),
+		DOC_090("090", "訂正発行登録書"),
+		DOC_100("100", "発行登録追補書類"),
+		DOC_110("110", "発行登録取下届出書"),
+		ANNUAL_REPORT
+		       ("120", "有価証券報告書"),
+		DOC_130("130", "訂正有価証券報告書"),
+		DOC_135("135", "確認書"),
+		DOC_136("136", "訂正確認書"),
+		QUARTERLY_REPORT
+		       ("140", "四半期報告書"),
+		DOC_150("150", "訂正四半期報告書"),
+		DOC_160("160", "半期報告書"),
+		DOC_170("170", "訂正半期報告書"),
+		DOC_180("180", "臨時報告書"),
+		DOC_190("190", "訂正臨時報告書"),
+		DOC_200("200", "親会社等状況報告書"),
+		DOC_210("210", "訂正親会社等状況報告書"),
+		DOC_220("220", "自己株券買付状況報告書"),
+		DOC_230("230", "訂正自己株券買付状況報告書"),
+		DOC_235("235", "内部統制報告書"),
+		DOC_236("236", "訂正内部統制報告書"),
+		DOC_240("240", "公開買付届出書"),
+		DOC_250("250", "訂正公開買付届出書"),
+		DOC_260("260", "公開買付撤回届出書"),
+		DOC_270("270", "公開買付報告書"),
+		DOC_280("280", "訂正公開買付報告書"),
+		DOC_290("290", "意見表明報告書"),
+		DOC_300("300", "訂正意見表明報告書"),
+		DOC_310("310", "対質問回答報告書"),
+		DOC_320("320", "訂正対質問回答報告書"),
+		DOC_330("330", "別途買付け禁止の特例を受けるための申出書"),
+		DOC_340("340", "訂正別途買付け禁止の特例を受けるための申出書"),
+		DOC_350("350", "大量保有報告書"),
+		DOC_360("360", "訂正大量保有報告書"),
+		DOC_370("370", "基準日の届出書"),
+		DOC_380("380", "変更の届出書");
+
+		public final String value;
+		public final String description;
+		
+		DocumentType(String value, String description) {
+			this.value       = value;
+			this.description = description;
+		}
+		
+		@Override
+		public String toString() {
+			return value;
+		}
+	}
+	
+	public static enum Status {
+		OK                   ("200"),
+		BAD_REQUEST          ("400"),
+		NOT_FOUND            ("404"),
+		INTERNAL_SERVER_ERROR("500");
+		
+		public final String value;
+		
+		Status(String value) {
+			this.value = value;
+		}
+		
+		@Override
+		public String toString() {
+			return value;
+		}
+	}
+				
+	public static enum Flag {
+		NO  ("0"),
+		YES ("1");
+		
+		public final String value;
+		
+		Flag(String value) {
+			this.value = value;
+		}
+		
+		@Override
+		public String toString() {
+			return value;
+		}
+	}
+
 	public static class List {
 		enum Type {
 			METADATA("1"),
@@ -74,24 +171,6 @@ public class API {
 				}
 			}
 			
-			public static enum Status {
-				OK                   ("200"),
-				BAD_REQUEST          ("400"),
-				NOT_FOUND            ("404"),
-				INTERNAL_SERVER_ERROR("500");
-				
-				public final String value;
-				
-				Status(String value) {
-					this.value = value;
-				}
-				
-				@Override
-				public String toString() {
-					return value;
-				}
-			}
-			
 			public String        title;
 			public Parameter     parameter;
 			@JSONName("resultset")
@@ -112,26 +191,9 @@ public class API {
 			public Metadata(JsonObject jsonObject) {
 				super(jsonObject);
 			}
-			
 		}
 
 		public static class Result extends JSONBase {
-			public static enum Flag {
-				NO  ("0"),
-				YES ("1");
-				
-				public final String value;
-				
-				Flag(String value) {
-					this.value = value;
-				}
-				
-				@Override
-				public String toString() {
-					return value;
-				}
-			}
-
 			public int    seqNumber;
 			public String docID;
 			public String edinetCode;
@@ -141,7 +203,7 @@ public class API {
 			public String fundCode;
 			public String ordinanceCode;
 			public String formCode;
-			public String docTypeCode;
+			public DocumentType docTypeCode;
 			public LocalDate periodStart;
 			public LocalDate periodEnd;
 			@DateTimeFormat("yyyy-MM-dd HH:mm")
@@ -194,7 +256,6 @@ public class API {
 			public Result(JsonObject jsonObject) {
 				super(jsonObject);
 			}
-
 		}
 
 		public static class Response extends JSONBase {
@@ -220,12 +281,82 @@ public class API {
 		}
 	}
 	
+	public static class Document {
+		public static final String HEADER_CONTENT_TYPE       = "Content-Type";
+		public static final String CONTENT_TYPE_OCTET_STREAM = "application/octet-stream";
+		public static final String CONTENT_TYPE_PDF          = "application/pdf";
+		public static final String CONTENT_TYPE_JSON         = "application/json; charset=utf-8";
+		
+		enum Type {
+			WHOLE  ("1"), // 提出本文書及び監査報告書
+			PDF    ("2"), // PDF
+			ATTACH ("3"), // 代替書面・添付文書
+			ENGLISH("4"); // 英文ファイル
+			
+			final String value;
+			
+			Type(String value) {
+				this.value = value;
+			}
+			
+			@Override
+			public String toString() {
+				return value;
+			}
+		}
+		
+		// v1/documents/S1234567?type=1
+		public static String getURL(Version version, String docId, Type type) {
+			return String.format("%s/%s/documents/%s?type=%s", URL_BASE, version.value, docId, type.value);
+		}
+		
+		public static byte[] getInstance(Version version, String docId, Type type) {
+			String url = getURL(version, docId, type);
+			HttpUtil.Result result = HttpUtil.getInstance().withRawData(true).download(url);
+			
+			String contentType = result.headerMap.get(HEADER_CONTENT_TYPE);
+			switch(contentType) {
+			case CONTENT_TYPE_OCTET_STREAM:
+			case CONTENT_TYPE_PDF:
+				return result.rawData;
+			case CONTENT_TYPE_JSON:
+				String message = new String(result.rawData, Charset.forName("UTF-8"));
+				logger.warn("Unexpected result {}", message);
+				return null;
+			default:
+				logger.error("Unexpected contentType {}!", contentType);
+				throw new UnexpectedException("Unknown valueType");
+			}
+		}
+		public static byte[] getInstance(String docId, Type type) {
+			return getInstance(Version.V1, docId, type);
+		}
+	}
 	
 	public static void main(String[] args) {
 		logger.info("START");
 		
-		List.Response response = List.getInstance(LocalDate.now(), List.Type.DATA);
-		logger.info("response {}", response);
+		LocalDate date = LocalDate.now();
+		
+		for(int i = 0; i < 1; i++) {
+			List.Response response = List.getInstance(date, List.Type.DATA);
+			logger.info("response {}", response);
+			for(List.Result e: response.results) {
+				if (e.docTypeCode == DocumentType.ANNUAL_REPORT || e.docTypeCode == DocumentType.QUARTERLY_REPORT) {
+					logger.info("{}  {}  {}  {}", e.docID, e.submitDateTime, e.docTypeCode.description, e.docDescription);
+				}
+			}
+			date = date.minusDays(1);
+		}
+		
+		{
+			byte[] result = Document.getInstance("S100I5LU", Document.Type.WHOLE);
+			if (result != null) {
+				logger.info("result {}", result.length);
+			} else {
+				logger.info("result null");
+			}
+		}
 		
 		logger.info("STOP");
 	}
