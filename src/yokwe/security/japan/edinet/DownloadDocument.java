c102819241678c4ba00b28e244b79cd80a5be9f2
hasegawa <hasegawa@ubuntudev.local>
2020-03-06 13:49:42 +0900

    Shuffle documentList to disguise download operation.

--- src/yokwe/security/japan/edinet/DownloadDocument.java
+++ src/yokwe/security/japan/edinet/DownloadDocument.java
@@ -1,6 +1,7 @@
 package yokwe.security.japan.edinet;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -25,17 +26,18 @@ public class DownloadDocument {
 				filter(o -> !dataFileMap.containsKey(o.docID)).
 				collect(Collectors.toList());
 		logger.info("documentList {}", documentList.size());
+		Collections.shuffle(documentList);
 		
 		int count = 0;
 		for(Document document: documentList) {
 			if ((count % 100) == 0) {
-				logger.info("{} {}", String.format("%5d / %5d", count, documentList.size()), document.submitDateTime);
+				logger.info("{} {}", String.format("%5d / %5d", count, documentList.size()), document.docID);
 			}
 			count++;
 			
 			byte[] data = API.Document.getInstance(document.docID, API.Document.Type.WHOLE);
 			if (data == null) {
-				logger.info("download failed {} {}", document.submitDateTime, document.docID);
+				logger.info("download failed {}", document.docID);
 			} else {
 				File file = Document.getDataFile(document.submitDateTime.toLocalDate(), document.docID);
 				FileUtil.rawWrite().file(file, data);
