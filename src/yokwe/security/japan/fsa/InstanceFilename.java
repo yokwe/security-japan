8c8329ff6c77b766f33f6dfdbe320d778ccf7fc5
hasegawa <hasegawa@ubuntudev.local>
2020-03-07 20:37:47 +0900

    Replace enum Ordinance with String.

--- src/yokwe/security/japan/fsa/InstanceFilename.java
+++ src/yokwe/security/japan/fsa/InstanceFilename.java
@@ -29,103 +29,26 @@ public class InstanceFilename implements Comparable<InstanceFilename> {
 			m.group("submitNo"),
 			m.group("submitDate")));
 
-	public static enum Ordinance {
-		CRP("crp"), // 企業内容等の開示に関する内閣府令
-		CTL("ctl"), // 財務計算に関する書類その他の情報の適正性を確保するための体制に関する内閣府令
-		SPS("sps"), // 特定有価証券の内容等の開示に関する内閣府令
-		TOO("too"), // 発行者以外の者による株券等の公開買付けの開示に関する内閣府令
-		TOI("toi"), // 発行者による上場株券等の公開買付けの開示に関する内閣府令
-		LVH("lvh"), // 株券等の大量保有の状況の開示に関する内閣府令
-		AUD("aud"), // 財務諸表等の監査証明に関する内閣府令
-		PFS("pfs"), // 日本基準財務諸表のうち本表に係る部分
-		IGP("igp"); // IFRS 財務諸表
-		
-		public final String value;
-		Ordinance(String value) {
-			this.value = value;
-		}
-		
-		@Override
-		public String toString() {
-			return value;
-		}
-		
-		private static final Ordinance[] VALUES = Ordinance.values();
-		public static Ordinance getInstance(String value) {
-			if (value == null || value.isEmpty()) return null;
-			for(Ordinance oridinance: VALUES) {
-				if (value.equals(oridinance.value)) return oridinance;
-			}
-			logger.error("Unknown value {}!", value);
-			throw new UnexpectedException("Unknown value");
-		}
-	}
-
-	public static enum Report {
-		SRS("srs"), // 有価証券届出書
-		ASR("asr"), // 有価証券報告書
-		DRS("drs"), // 有価証券報告書【みなし有価証券届出書】
-		QSR("qsr"), // 四半期報告書
-		SSR("ssr"), // 中間期報告書
-		ESR("esr"), // 臨時報告書
-		RST("rst"), // 発行登録書
-		REP("rep"), // 発行登録追補書類
-		SBR("sbr"), // 自己株券買付状況報告書
-		TON("ton"), // 公開買付届出書
-		PST("pst"), // 意見表明報告書
-		WTO("wto"), // 公開買付撤回届出書
-		TOR("tor"), // 公開買付報告書
-		TOA("toa"), // 対質問回答報告書
-		LVH("lvh"), // 大量保有報告書
-		ICR("icr"), // 内部統制報告書
-		
-		Q1R("q1r"), // 第１四半期報告書
-		Q2R("q2r"), // 第２四半期報告書
-		Q3R("q3r"), // 第３四半期報告書
-		Q4R("q4r"), // 第４四半期報告書
-		Q5R("q5r"); // 第５四半期報告書
-		
-		public final String value;
-		Report(String value) {
-			this.value = value;
-		}
-		
-		@Override
-		public String toString() {
-			return value;
-		}
-		
-		private static final Report[] VALUES = Report.values();
-		public static Report getInstance(String value) {
-			if (value == null || value.isEmpty()) return null;
-			for(Report report: VALUES) {
-				if (value.equals(report.value)) return report;
-			}
-			logger.error("Unknown value {}!", value);
-			throw new UnexpectedException("Unknown value");
-		}
-	}
-	
-	public final Ordinance ordinance;
-	public final String    form;
-	public final Report    report;
-	public final String    reportNo;
-	public final String    code;
-	public final String    extraNo;
-	public final String    date;
-	public final String    submitNo;
-	public final String    submitDate;
-	public final String    string;
+	public final String  ordinance;
+	public final String  form;
+	public final Report  report;
+	public final String  reportNo;
+	public final String  code;
+	public final String  extraNo;
+	public final String  date;
+	public final String  submitNo;
+	public final String  submitDate;
+	public final String  string;
 	
 	public InstanceFilename(String ordinance, String form, String report, String reportNo, String code, String extraNo, String date, String submitNo, String submitDate) {
-		this.ordinance = Ordinance.getInstance(ordinance);
-		this.form = form;
-		this.report = Report.getInstance(report);
-		this.reportNo = reportNo;
-		this.code = code;
-		this.extraNo = extraNo;
-		this.date = date;
-		this.submitNo = submitNo;
+		this.ordinance  = ordinance;
+		this.form       = form;
+		this.report     = Report.getInstance(report);
+		this.reportNo   = reportNo;
+		this.code       = code;
+		this.extraNo    = extraNo;
+		this.date       = date;
+		this.submitNo   = submitNo;
 		this.submitDate = submitDate;
 		
 		this.string = String.format("jp%s%s-%s-%s_%s-%s_%s_%s_%s.xbrl", ordinance, form, report, reportNo, code, extraNo, date, submitNo, submitDate);
@@ -146,6 +69,7 @@ public class InstanceFilename implements Comparable<InstanceFilename> {
 	@Override
 	public int compareTo(InstanceFilename that) {
 		// jpcrp040300-q3r-001_E01442-000_2020-01-31_01_2020-03-06.xbrl
+		//                     111111 444            33 2222222222
 		int ret = this.code.compareTo(that.code);
 		if (ret == 0) ret = this.submitDate.compareTo(that.submitDate);
 		if (ret == 0) ret = this.submitNo.compareTo(submitNo);
