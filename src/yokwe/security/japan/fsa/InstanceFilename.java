023bd07bd9780214e6da690c3f05de828606453e
hasegawa <hasegawa@ubuntudev.local>
2020-03-07 14:23:07 +0900

    Use extraNo in compareTo()

--- src/yokwe/security/japan/fsa/InstanceFilename.java
+++ src/yokwe/security/japan/fsa/InstanceFilename.java
@@ -1,7 +1,6 @@
 package yokwe.security.japan.fsa;
 
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -14,8 +13,8 @@ public class InstanceFilename implements Comparable<InstanceFilename> {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(InstanceFilename.class);
 
 	// jp{府令略号}{様式番号}-{報告書略号}-{報告書連番(3 桁)}_{EDINET コード又はファンドコード}-{追番(3 桁)}_{報告対象期間期末日|報告義務発生日}_{報告書提出回数(2 桁)}_{報告書提出日}.xbrl 
-	// jpcrp040300-q3r-001_E01442-000_2020-01-31_01_2020-03-06.xbrl
 	// jp crp 040300     -q3r        -001             _E01442                      -000        _2020-01-31                   _01                 _2020-03-06.xbrl
+	// jpcrp040300-q3r-001_E01442-000_2020-01-31_01_2020-03-06.xbrl
 	
 	private static final Pattern PAT = Pattern.compile("jp(?<ordinance>[a-z]+)(?<form>[0-9]{6})-(?<report>.+?)-(?<reportNo>[0-9]{3})_(?<code>[EG][0-9]{5})-(?<extraNo>[0-9]{3})_(?<date>20[0-9]{2}-[01][0-9]-[0-3][0-9])_(?<submitNo>[0-9]{2})_(?<submitDate>20[0-9]{2}-[01][0-9]-[0-3][0-9]).xbrl");
 
@@ -150,6 +149,7 @@ public class InstanceFilename implements Comparable<InstanceFilename> {
 		int ret = this.code.compareTo(that.code);
 		if (ret == 0) ret = this.submitDate.compareTo(that.submitDate);
 		if (ret == 0) ret = this.submitNo.compareTo(submitNo);
+		if (ret == 0) ret = this.extraNo.compareTo(extraNo);
 		return ret;
 	}
 	@Override
@@ -161,36 +161,5 @@ public class InstanceFilename implements Comparable<InstanceFilename> {
 		} else {
 			return false;
 		}
-	}
-
-	
-	public static void main(String[] args) {
-		logger.info("START");
-		
-		{
-			String filename = "jpcrp040300-q3r-001_E01442-000_2020-01-31_01_2020-03-06.xbrl";
-			logger.info("filename   {}", filename);
-			Matcher m = PAT.matcher(filename);
-			if (m.find()) {
-				logger.info("ordinance  {}", m.group("ordinance"));
-				logger.info("form       {}", m.group("form"));
-				logger.info("report     {}", m.group("report"));
-				logger.info("reportNo   {}", m.group("reportNo"));
-				logger.info("code       {}", m.group("code"));
-				logger.info("extraNo    {}", m.group("extraNo"));
-				logger.info("date       {}", m.group("date"));
-				logger.info("submitNo   {}", m.group("submitNo"));
-				logger.info("submitDate {}", m.group("submitDate"));
-			} else {
-				logger.info("XXX");
-			}
-		}
-		{
-			String filename = "jpcrp040300-q3r-001_E01442-000_2020-01-31_01_2020-03-06.xbrl";
-			InstanceFilename instanceFilename = InstanceFilename.getInstance(filename);
-			logger.info("filename {}", filename);
-			logger.info("         {}", instanceFilename);
-		}
-		logger.info("STOP");
 	}
 }
