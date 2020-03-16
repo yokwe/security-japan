03f455fe4fb9f47537925bf67a134279cc23cc41
hasegawa <hasegawa@ubuntudev.local>
2020-03-16 10:30:35 +0900

    Change double to String for missing value.

--- src/yokwe/security/japan/data/StockPrice.java
+++ src/yokwe/security/japan/data/StockPrice.java
@@ -8,49 +8,91 @@ import java.util.List;
 import yokwe.util.CSVUtil;
 
 public class StockPrice implements Comparable<StockPrice> {
-	public static final String PATH_DIR_DATA = "tmp/data/stock-price";
-	public static String getPath(String stockCode) {
-		return String.format("%s/%s.csv", PATH_DIR_DATA, stockCode);
-	}
+	public static final String PATH_FILE = "tmp/data/stock-price.csv";
 
+	public static List<StockPrice> getList() {
+		List<StockPrice> ret = CSVUtil.read(StockPrice.class).file(PATH_FILE);
+		if (ret == null) {
+			ret = new ArrayList<>();
+		}
+		return ret;
+	}
 	public static void save(Collection<StockPrice> collection) {
 		save(new ArrayList<>(collection));
 	}
 	public static void save(List<StockPrice> list) {
-		if (list.isEmpty()) return;
-		String stockCode = list.get(0).stockCode;
-		String path      = getPath(stockCode);
-		
 		// Sort before save
 		Collections.sort(list);
-		CSVUtil.write(StockPrice.class).file(path, list);
+		CSVUtil.write(StockPrice.class).file(PATH_FILE, list);
 	}
 	
 
+	public String time;      // HH:mm -- taken from file modified time
 	public String stockCode; // 5 digits
-	public String time;      // HH:mm:ss -- taken from file modified time
 	
-	public double price;
+	public String price;
 	public String priceTime; // HH:mm or empty
 	
-	public double sell;
+	public String sell;
 	public String sellTime;  // HH:mm or empty
 	
-	public double buy;
+	public String buy;
 	public String buyTime;   // HH:mm or empty
 	
-	public double open;
-	public double high;
-	public double low;
-	public double close;
-	public long   volume;
-	public long   tradeM;
+	public String open;
+	public String high;
+	public String low;
+
+	public String volume;
+	public String trade;
 	
+	public StockPrice(
+			String time,
+			String stockCode,
+			
+			String price,
+			String priceTime,
+			
+			String sell,
+			String sellTime,
+			
+			String buy,
+			String buyTime,
+			
+			String open,
+			String high,
+			String low,
+			
+			String volume,
+			String trade
+		) {
+		this.time = time;
+		this.stockCode = stockCode;
+		
+		this.price = price;
+		this.priceTime = priceTime;
+		
+		this.sell = sell;
+		this.sellTime = sellTime;
+		
+		this.buy = buy;
+		this.buyTime = buyTime;
+		
+		this.open = open;
+		this.high = high;
+		this.low = low;
+		
+		this.volume = volume;
+		this.trade = trade;
+	}
+	public StockPrice() {
+		this(null, null, null, null, null, null, null, null, null, null, null, null, null);
+	}
 	
 	@Override
 	public int compareTo(StockPrice that) {
-		int ret = this.stockCode.compareTo(that.stockCode);
-		if (ret == 0) ret = this.time.compareTo(that.time);
+		int ret = this.time.compareTo(that.time);
+		if (ret == 0) ret = this.stockCode.compareTo(that.stockCode);
 		return ret;
 	}
 }
