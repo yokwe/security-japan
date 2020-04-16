db5663f647d1926b09946ed0b7221499608d76b0
hasegawa <hasegawa@ubuntudev.local>
2020-04-16 23:14:28 +0900

    Update with correct url

--- src/yokwe/security/japan/sony/MSFundCode.java
+++ src/yokwe/security/japan/sony/MSFundCode.java
@@ -1,10 +1,10 @@
 package yokwe.security.japan.sony;
 
 public class MSFundCode {
-	public static final String URL = "https://moneykit.net/visitor/fund/fund_notice/notice_JP90C0009VE0.html";
+	public static final String URL = "https://apl.morningstar.co.jp/webasp/sonybk/detail/JP90C0009VE0.html";
 	
 	public static final String getURL(String isinCode) {
-		return String.format("https://moneykit.net/visitor/fund/fund_notice/notice_%s.html", isinCode);
+		return String.format("https://apl.morningstar.co.jp/webasp/sonybk/detail/%s.html", isinCode);
 	}
 	// To get msFundCode from isinCode, fetch page using above getURL() and find line like "var msFundCode = '2013121001';"
 
