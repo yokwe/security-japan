9e0b527799f6f34f24e0cdd01397ad9298def545
hasegawa <hasegawa@ubuntudev.local>
2020-04-17 11:00:43 +0900

    Put class in separate file (Company and Currency)

---
+++ src/yokwe/security/japan/sony/Currency.java
@@ -0,0 +1,14 @@
+package yokwe.security.japan.sony;
+
+import yokwe.util.EnumUtil;
+
+public enum Currency {
+	AUD,
+	CAD,
+	JPY,
+	NZD,
+	USD;
+	public static Currency get(String value) {
+		return EnumUtil.getInstance(Currency.class, value);
+	}
+}
