ed6f13824be1cb99e841eb1cd9e4910ac0a08ab9
hasegawa <hasegawa@ubuntudev.local>
2020-04-19 09:38:31 +0900

    Process all funds using FundInfo.

--- src/yokwe/security/japan/sony/Price.java
+++ src/yokwe/security/japan/sony/Price.java
@@ -1,6 +1,5 @@
 package yokwe.security.japan.sony;
 
-import java.io.ByteArrayInputStream;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -8,18 +7,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import javax.xml.bind.JAXB;
-
-import org.slf4j.LoggerFactory;
-
-import yokwe.security.japan.sony.price.MorningstarXML;
 import yokwe.util.CSVUtil;
-import yokwe.util.FileUtil;
-import yokwe.util.StringUtil;
 
 public class Price implements Comparable<Price> {
-	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Price.class);
-
 	public static final String PATH_DIR_DATA = "tmp/data/sony/price";
 	public static String getPath(String stockCode) {
 		return String.format("%s/%s.csv", PATH_DIR_DATA, stockCode);
@@ -52,14 +42,15 @@ public class Price implements Comparable<Price> {
 	public BigDecimal price;
 	public long       volume;
 	
-	public Price(LocalDate date, String isinCode, BigDecimal price, long volume) {
+	public Price(LocalDate date, String isinCode, Currency currency, BigDecimal price, long volume) {
 		this.date     = date;
 		this.isinCode = isinCode;
+		this.currency = currency;
 		this.price    = price;
 		this.volume   = volume;
 	}
 	public Price() {
-		this(null, null, null, 0);
+		this(null, null, null, null, 0);
 	}
 
 	@Override
@@ -67,18 +58,5 @@ public class Price implements Comparable<Price> {
 		int ret = this.date.compareTo(that.date);
 		if (ret == 0) ret = this.isinCode.compareTo(that.isinCode);
 		return ret;
-	}
-
-	
-	public static void main(String[] args) {
-		logger.info("START");
-		
-		byte[] byetArray = FileUtil.rawRead().file("tmp/2013121001.xml");
-		logger.info("byetArray {}!", byetArray.length);
-		
-		MorningstarXML morningstarXML = JAXB.unmarshal(new ByteArrayInputStream(byetArray), MorningstarXML.class);
-		logger.info("morningstarXML {}", StringUtil.toString(morningstarXML));
-		
-		logger.info("STOP");
 	}
 }
--- src/yokwe/security/japan/sony/UpdatePrice.java
+++ src/yokwe/security/japan/sony/UpdatePrice.java
@@ -1,26 +1,85 @@
 package yokwe.security.japan.sony;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.JAXB;
 
 import org.slf4j.LoggerFactory;
 
+import yokwe.security.japan.sony.price.Day;
+import yokwe.security.japan.sony.price.Month;
 import yokwe.security.japan.sony.price.MorningstarXML;
+import yokwe.security.japan.sony.price.Year;
 import yokwe.util.FileUtil;
-import yokwe.util.StringUtil;
+import yokwe.util.HttpUtil;
 
 public class UpdatePrice {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(UpdatePrice.class);
 
+	//https://apl.morningstar.co.jp/xml/chart/funddata/2013121001.xml
+	
+	private static String URL_BASE_FUNDDATA = "https://apl.morningstar.co.jp/xml/chart/funddata";
+	public static String getURL(String msFundCode) {
+		return String.format("%s/%s.xml", URL_BASE_FUNDDATA, msFundCode);
+	}
+	
+	public static final String PATH_DIR_DATA = "tmp/download/sony/page";
+	public static String getPath(String msFundCode) {
+		return String.format("%s/%s.xml", PATH_DIR_DATA, msFundCode);
+	}
+
 	public static void main(String[] arsg) {
 		logger.info("START");
 
-		byte[] byetArray = FileUtil.rawRead().file("tmp/2013121001.xml");
-		logger.info("byetArray {}!", byetArray.length);
-		
-		MorningstarXML morningstarXML = JAXB.unmarshal(new ByteArrayInputStream(byetArray), MorningstarXML.class);
-		logger.info("morningstarXML {}", StringUtil.toString(morningstarXML));
+		for(FundInfo e: FundInfo.getList()) {
+			String msFundCode = e.msFundCode;
+			if (msFundCode.isEmpty()) continue;
+			
+			logger.info("{} {}", e.isinCode, msFundCode);
+			
+			byte[] byteArray;
+			{
+				String path = getPath(msFundCode);
+				File file = new File(path);
+				if (file.exists()) {
+					byteArray = FileUtil.rawRead().file(file);
+				} else {
+					String url = getURL(msFundCode);
+					logger.info("  download {}", url);
+					HttpUtil.Result result = HttpUtil.getInstance().withRawData(true).download(url);
+					byteArray = result.rawData;
+					FileUtil.rawWrite().file(file, byteArray);
+				}
+			}
+			
+			List<Price> priceList = new ArrayList<>();
+			{
+				MorningstarXML morningstarXML = JAXB.unmarshal(new ByteArrayInputStream(byteArray), MorningstarXML.class);
+//				logger.info("morningstarXML {}", StringUtil.toString(morningstarXML));
+				for(Year year: morningstarXML.fund.yearList) {
+					for(Month month: year.monthList) {
+						for(Day day: month.dayList) {
+							if (day.price.isEmpty())  continue;
+							if (day.volume.isEmpty()) continue;
+
+							// 	public Price(LocalDate date, String isinCode, BigDecimal price, long volume) {
+							LocalDate  date = LocalDate.parse(String.format("%s-%s-%s", day.year, day.month, day.value));
+							BigDecimal value = new BigDecimal(day.price);
+							long       volume = Long.parseLong(day.volume);
+							Price price = new Price(date, e.isinCode, e.currency, value, volume);
+							priceList.add(price);
+						}
+					}
+				}
+			}
+			logger.info("  save {} {}", priceList.size(), Price.getPath(e.isinCode));
+			Price.save(priceList);
+		}
 
 		logger.info("STOP");
 	}
